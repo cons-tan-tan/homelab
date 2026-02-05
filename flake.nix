fffb6c195c075ce8a2e0015943e0fa8fcea5e745
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2026-02-05 23:55:07 +0900

    feat: add Nix flake for development environment
    Introduce a reproducible dev shell using Nix flakes with:
    - Terraform 1.14.4 via nixpkgs-terraform
    - Google Cloud SDK
    - sops for secret management

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs-terraform.url = "github:stackbuilders/nixpkgs-terraform";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs-terraform,
+      nixpkgs,
+      systems,
+    }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          terraform = nixpkgs-terraform.packages.${system}."terraform-1.14.4";
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              terraform
+              pkgs.google-cloud-sdk
+              pkgs.sops
+            ];
+          };
+        }
+      );
+    };
+}
