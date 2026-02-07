e01cc50d1251b403141b5d9c9085896e9ed44a00
cons-tan-tan <132136681+cons-tan-tan@users.noreply.github.com>
2026-02-08 04:20:23 +0900

    feat: add pve02 node and expand cluster VMs
    Add second Proxmox node (pve02) to node_list and restructure VM
    definitions: rename k8s-cp to k8s-cp-02 on pve02, add k8s-wk-02
    on pve02 with 10 cores / 72GB RAM / 200GB disk, and update
    k8s-wk-01 specs (6 cores / 24GB RAM / 100GB disk). Cluster
    endpoint moved to 192.168.2.12.

--- terraform/proxmox/locals.tf
+++ terraform/proxmox/locals.tf
@@ -8,7 +8,7 @@ locals {
 
   cluster = {
     name     = "homelab"
-    endpoint = "https://192.168.2.11:6443"
+    endpoint = "https://192.168.2.12:6443"
   }
 
   node_list = {
@@ -17,24 +17,38 @@ locals {
       ip           = "192.168.1.1"
       datastore_id = "local"
     }
+    "pve02" = {
+      name         = "pve02"
+      ip           = "192.168.1.2"
+      datastore_id = "local"
+    }
   }
 
   vm_list = {
-    "k8s-cp" = {
-      node_name = local.node_list.pve01.name
-      vm_id     = 1001
-      cpu_cores = 2
+    "k8s-cp-02" = {
+      node_name = local.node_list.pve02.name
+      vm_id     = 2001
+      cpu_cores = 4
       memory    = 8192
-      disk_size = 30
-      ip        = "192.168.2.11"
+      disk_size = 50
+      ip        = "192.168.2.12"
       type      = "controlplane"
     }
-    "k8s-wk" = {
+    "k8s-wk-02" = {
+      node_name = local.node_list.pve02.name
+      vm_id     = 2101
+      cpu_cores = 10
+      memory    = 73728 # 72GB
+      disk_size = 200
+      ip        = "192.168.2.22"
+      type      = "worker"
+    }
+    "k8s-wk-01" = {
       node_name = local.node_list.pve01.name
       vm_id     = 1101
       cpu_cores = 6
-      memory    = 16384
-      disk_size = 30
+      memory    = 24576 # 24GB
+      disk_size = 100
       ip        = "192.168.2.21"
       type      = "worker"
     }
